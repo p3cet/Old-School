c6d0aaa3a856bd08b488dd48d0bd3a830d86263b
p3cet <cprzem@gmail.com>
2018-09-23 13:08:31 +0200

    Add files via upload

---
+++ Plasma/metaballGL/Metaball.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+
+namespace metaball
+{
+    class Metaball
+    {
+        public Tuple<int,int> Speed {get;private set;}
+        public Color[,] Body { get; private set;}
+        public Tuple<int, int> Position {get; private set;}
+
+        private MetaballEngine engine;
+        
+        public Metaball(int size, int x, int y, Color clr,int speed, int angle, MetaballEngine engine)
+        {
+            if (size<3) size=3;
+            if (size%2==0) size++;   //make sure, size is not even
+            Speed = new Tuple<int,int>(speed,angle%360);
+            Body = new Color[size, size];
+            Position = new Tuple<int, int>(x, y);
+            GenerateBody(size, clr);
+            this.engine = engine;
+        }
+
+        public void Move()
+        {
+            int dx, dy;
+            int x, y;
+            if (Speed.Item2<90)
+            {
+                dx = -(int)(Math.Cos(DegToRad(Speed.Item2)) * Speed.Item1);
+                dy = -(int)(Math.Sin(DegToRad(Speed.Item2)) * Speed.Item1);
+            }
+            else if (Speed.Item2>=90 && Speed.Item2<180)
+            {
+                dx = (int)(Math.Cos(DegToRad(180-Speed.Item2)) * Speed.Item1);
+                dy = -(int)(Math.Sin(DegToRad(180-Speed.Item2)) * Speed.Item1);
+            }
+            else if (Speed.Item2 >= 180 && Speed.Item2 < 270)
+            {
+                dx = (int)(Math.Cos(DegToRad(Speed.Item2-180)) * Speed.Item1);
+                dy = (int)(Math.Sin(DegToRad(Speed.Item2-180)) * Speed.Item1);
+    
+            }
+            else //angle>=270 && angle<360
+            {
+                dx = -(int)(Math.Cos(DegToRad(360 - Speed.Item2)) * Speed.Item1);
+                dy = (int)(Math.Sin(DegToRad(360 - Speed.Item2)) * Speed.Item1);
+
+            }
+
+            /*if (Speed.Item2 < 90)
+            {
+                dx = -(int)(engine.Cos[Speed.Item2] * Speed.Item1);
+                dy = -(int)(engine.Sin[Speed.Item2] * Speed.Item1);
+            }
+            else if (Speed.Item2 >= 90 && Speed.Item2 < 180)
+            {
+                dx = (int)(engine.Cos[180 - Speed.Item2] * Speed.Item1);
+                dy = -(int)(engine.Sin[180 - Speed.Item2] * Speed.Item1);
+            }
+            else if (Speed.Item2 >= 180 && Speed.Item2 < 270)
+            {
+                dx = (int)(engine.Cos[Speed.Item2 - 180] * Speed.Item1);
+                dy = (int)(engine.Sin[Speed.Item2 - 180] * Speed.Item1);
+
+            }
+            else //angle>=270 && angle<360
+            {
+                dx = -(int)(engine.Cos[360 - Speed.Item2] * Speed.Item1);
+                dy = (int)(engine.Sin[360 - Speed.Item2] * Speed.Item1);
+
+            }*/
+
+            x = Position.Item2 + dx;
+            y = Position.Item1 + dy;
+            //loop coords
+            if (x < 0) x += engine.World.GetLength(0);
+            else if (x >= engine.World.GetLength(0)) x -= engine.World.GetLength(0);
+            if (y < 0) y += engine.World.GetLength(0);
+            else if (y >= engine.World.GetLength(0)) y -= engine.World.GetLength(0);
+
+
+            Position = new Tuple<int, int>(y, x);
+        }
+
+        private double DegToRad(double angle)
+        {
+            return angle * Math.PI / 180;
+        }
+
+        
+        private void GenerateBody(int size, Color clr)
+        {
+            //center is in size/2,size/2
+
+            double radius;
+            double fr;
+
+            for (int i=0;i<=size/2;i++)
+            {
+                for (int j=0;j<=size/2;j++)
+                {
+                    radius = Math.Sqrt((size / 2 - i) * (size / 2 - i) + (size / 2 - j) * (size / 2 - j));
+                    radius = radius / (size / 2);
+                    if (radius <= 1)
+                    {
+                        if (radius < 1/3)
+                            fr = 1 - 3 * radius * radius;
+                        else
+                            fr = (3 / 2) * (1 - radius) * (1 - radius);
+
+                        Body[i, j] = Color.FromRgb((byte)(clr.R * fr), (byte)(clr.G * fr), (byte)(clr.B * fr));
+                        Body[i, size - 1 - j] = Color.FromRgb((byte)(clr.R * fr), (byte)(clr.G * fr), (byte)(clr.B * fr));
+                        Body[size - 1 - i, j] = Color.FromRgb((byte)(clr.R * fr), (byte)(clr.G * fr), (byte)(clr.B * fr));
+                        Body[size - 1 - i, size - 1 - j] = Color.FromRgb((byte)(clr.R * fr), (byte)(clr.G * fr), (byte)(clr.B * fr));
+                    }
+                }
+            }
+        }
+    }
+
+    class MetaballEngine
+    {
+        public Color[,] World { get; private set; }
+        public LinkedList<Metaball> MetaballList { get; private set;}
+        public int Threshold { get; private set; }              //don't render cell below  this value
+        public float[] Sin { get; private set; }
+        public float[] Cos { get; private set; }
+
+        public MetaballEngine(int size, int threshold)
+        {
+            MetaballList = new LinkedList<Metaball>();
+            World = new Color[size, size];
+            Threshold = threshold;
+            GenerateSin();
+            GenerateCos();
+        }
+        
+        public Color[,] CalculateFrame()
+        {
+            int maxI, maxJ;
+            int x, y;
+            int posX, posY;
+            int size=World.GetLength(0);
+            World = new Color[size, size];
+            foreach (Metaball ball in MetaballList)
+            {
+                ball.Move();           
+                maxI = ball.Body.Length / ball.Body.GetLength(0);
+                maxJ = ball.Body.GetLength(0);
+                posX=ball.Position.Item2;
+                posY=ball.Position.Item1;
+                for (int i = 0; i < maxI; i++)
+                {
+                    y = i - posY;
+                    if (y < 0) y += World.GetLength(0);
+                    else if (y >= World.GetLength(0)) y -= World.GetLength(0);
+                    for (int j = 0; j < maxJ; j++)
+                    {
+                        x = j - posX;
+                        if (x < 0) x += World.GetLength(0);
+                        else if (x >= World.GetLength(0)) x -= World.GetLength(0);
+                        World[y, x] +=ball.Body[i, j];
+                        //Console.WriteLine(World[y, x].R + " " + World[y, x].G + " " + World[y, x].B);
+                    }
+                }
+            }
+            return World;
+        }
+
+        public void AddMetaball(Metaball ball)
+        {
+            MetaballList.AddLast(ball);
+        }
+
+        private void GenerateSin()
+        {
+            Sin = new float[91];
+            for (int i = 0; i < 91; i++)
+                Sin[i] = (float)Math.Sin(DegToRad(i));
+        }
+
+        private void GenerateCos()
+        {
+            Cos = new float[91];
+            for (int i = 0; i < 91; i++)
+                Cos[i] = (float)Math.Cos(DegToRad(i));
+        }
+
+        private double DegToRad(double angle)
+        {
+            return angle * Math.PI / 180;
+        }
+    }
+
+}
---
+++ Plasma/metaballGL/SharpGLForm.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using SharpGL;
+using metaball;
+
+namespace metaballGL
+{
+    /// <summary>
+    /// The main form class.
+    /// </summary>
+    public partial class SharpGLForm : Form
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SharpGLForm"/> class.
+        /// </summary>
+
+        private Metaball TestBall;
+        private Metaball TestBall2;
+        private Metaball TestBall3;
+        private Metaball TestBall4;
+        private Metaball TestBall5;
+
+        private MetaballEngine Engine;
+        
+        public SharpGLForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Handles the OpenGLDraw event of the openGLControl control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RenderEventArgs"/> instance containing the event data.</param>
+        private void openGLControl_OpenGLDraw(object sender, RenderEventArgs e)
+        {
+            //  Get the OpenGL object.
+            OpenGL gl = openGLControl.OpenGL;
+
+            //  Clear the color and depth buffer.
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+
+            //  Load the identity matrix.
+            gl.LoadIdentity();
+            gl.PointSize(2.0f);
+            Engine.CalculateFrame();
+            gl.Begin(OpenGL.GL_POINTS);           
+            for (int i = 0; i < Engine.World.Length / Engine.World.GetLength(0); i++)
+                for (int j = 0; j < Engine.World.GetLength(0); j++)
+                    if ((Engine.World[i, j].R + Engine.World[i, j].G + Engine.World[i, j].B) / 3 > Engine.Threshold)
+                    {
+                        gl.Color(Engine.World[i, j].R, Engine.World[i, j].G, Engine.World[i, j].B);
+                        gl.Vertex(j, i);
+                    }
+            gl.End();
+            
+        }
+
+
+
+        /// <summary>
+        /// Handles the OpenGLInitialized event of the openGLControl control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        private void openGLControl_OpenGLInitialized(object sender, EventArgs e)
+        {
+            //  TODO: Initialise OpenGL here.
+
+            //  Get the OpenGL object.
+            OpenGL gl = openGLControl.OpenGL;
+
+            //  Set the clear color.
+            gl.ClearColor(0, 0, 0, 0);
+
+            System.Windows.Media.Color clr =System.Windows.Media.Color.FromRgb(255,0,0);
+            System.Windows.Media.Color clr2 = System.Windows.Media.Color.FromRgb(0, 255, 0);
+            System.Windows.Media.Color clr3 = System.Windows.Media.Color.FromRgb(0, 0, 255);
+            System.Windows.Media.Color clr4 = System.Windows.Media.Color.FromRgb(255, 255, 0);
+            System.Windows.Media.Color clr5 = System.Windows.Media.Color.FromRgb(0, 255, 255);
+            Engine = new MetaballEngine(300, 30);
+            TestBall = new Metaball(151, 200,0,clr,6,95,Engine);
+            TestBall2 = new Metaball(121, 50, 0, clr2, 3, 270, Engine);
+            TestBall3 = new Metaball(151, 100, 0, clr3, 6, 135, Engine);
+            TestBall4 = new Metaball(191, 100, 0, clr4, 9, 45, Engine);
+            TestBall5 = new Metaball(191, 100, 0, clr5, 9, 75, Engine);
+
+
+            Engine.AddMetaball(TestBall);
+            Engine.AddMetaball(TestBall2);
+            Engine.AddMetaball(TestBall3);
+            Engine.AddMetaball(TestBall4);
+            Engine.AddMetaball(TestBall5);
+            
+            //Engine.CalculateFrame();
+        }
+
+        /// <summary>
+        /// Handles the Resized event of the openGLControl control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        private void openGLControl_Resized(object sender, EventArgs e)
+        {
+            //  TODO: Set the projection matrix here.
+
+            //  Get the OpenGL object.
+            OpenGL gl = openGLControl.OpenGL;
+
+            //  Set the projection matrix.
+            gl.MatrixMode(OpenGL.GL_PROJECTION);
+
+            //  Load the identity.
+            gl.LoadIdentity();
+
+            gl.Ortho2D(0, 300, 0, 300);
+
+            //  Set the modelview matrix.
+            gl.MatrixMode(OpenGL.GL_MODELVIEW);
+        }
+
+        private void SharpGLForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
